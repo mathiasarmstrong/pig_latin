475634bec13049d3bfe28c6a2e10ace239407123
Jered McCullough <mathias.armstrong@gmail.com>
2014-06-17 09:22:14 -0700

    I converted it to a module as per instructions

--- pig_latin.rb
+++ pig_latin.rb
@@ -1,18 +1,19 @@
 
-
-def pig_latin( user_input )
-	pig_latinized_words = user_input.split( ' ' ).map do |word|
-		front, back, vwl = "","","aeiou"
-		( front = $&; word.sub!( /\A\W+/, "" ); ) if /\A\W+/ === word
-		( back = $&; word.sub!( /\W+\z/, "" ); ) if /\W+\z/ === word
-		case word
-		when /\A[#{vwl}]/i
-			word + 'ay'
-		when /\A[^#{vwl}]/i 
-			word = word.sub( /\Ay|\Aqu|\A[^#{vwl}yq]+/i, "" ) + $& + "ay"
-		end     
-			word = front + word + back
-		end
-	pig_latinized_words = pig_latinized_words.join( " " )
-	return pig_latinized_words
+module pig_latin_translator
+	def self.pig_latin( user_input )
+		pig_latinized_words = user_input.split( ' ' ).map do |word|
+			front, back, vwl = "","","aeiou"
+			( front = $&; word.sub!( /\A\W+/, "" ); ) if /\A\W+/ === word
+			( back = $&; word.sub!( /\W+\z/, "" ); ) if /\W+\z/ === word
+			case word
+			when /\A[#{vwl}]/i
+				word + 'ay'
+			when /\A[^#{vwl}]/i
+				word = word.sub( /\Ay|\Aqu|\A[^#{vwl}yq]+/i, "" ) + $& + "ay"
+			end
+				word = front + word + back
+			end
+		pig_latinized_words = pig_latinized_words.join( " " )
+		return pig_latinized_words
+	end
 end
